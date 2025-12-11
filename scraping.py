c813756edd03f88a909a4c342432d1b3fb663920
Harishmitha07 <24it038@kpriet.ac.in>
2025-12-11 14:33:10 +0530

    Refactor scraping.py to save data in multiple formats
    Enhanced the web scraping script to extract book details and save them to CSV, Excel, and SQLite formats.

--- scraping.py
+++ scraping.py
@@ -1,16 +1,50 @@
 import requests
 from bs4 import BeautifulSoup
-response=requests.get("https://books.toscrape.com/")
-response.encoding='utf-8'
+import pandas as pd
+import sqlite3
+import re
+
+
+url = "https://books.toscrape.com/"
+response = requests.get(url)
+print(url)
+print(response)
 
 soup = BeautifulSoup(response.text, "html.parser")
-soup2=soup.find_all('article',class_="product_pod")
-for book in soup2:
-   name=book.find('h3').a.text
-   print(name)
-   price=book.find('div',class_="product_price").find('p',class_='price_color').text.replace(' £', ' ')
-   print(price)
-   rating=book.find('p',class_="star-rating")
-   rating_text=rating["class"][1]
-   print(rating_text)
+books = soup.select("article.product_pod")
+
+data = []
+
+for book in books:
+
+    name = book.select_one("h3 a")["title"]
+
+    
+    price = book.select_one(".price_color").text
+    price_value = float(re.sub(r"[^\d.]", "", price))   # remove £, Â
+
+   
+    rating_class = book.select_one("p.star-rating")["class"][1]
+
+   
+    data.append({
+        "Name": name,
+        "Price": price_value,
+        "Rating": rating_class
+    })
+
+
+df = pd.DataFrame(data)
+
+
+df.to_csv("books_output.csv", index=False, encoding="utf-8")
+
+print("Scraping completed. CSV file saved as books_output.csv")
+df.to_excel("books_output.xlsx", index=False)
+conn=sqlite3.connect("books_pd.db")
+df.to_sql("books_table",conn,if_exists="replace",index=False)
+conn.close()
+print("saved to:books_output.csv,books_output.xlsx,books.db")
+print(df.head())
+
 
